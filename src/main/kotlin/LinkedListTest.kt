cf91b548a5e42ba2d251d2f4a71f0fc06abcc42e
vikashbkj <1992.vikash@gmail.com>
2025-08-03 02:08:08 +0530

    Removed commented code from LinkedListTest.kt

--- src/main/kotlin/LinkedListTest.kt
+++ src/main/kotlin/LinkedListTest.kt
@@ -27,35 +27,12 @@ class LinkedListTest<T> {
         println()
     }
 
-/*    internal fun insertAtBeginning(value: T) {
-        val node = ListNode(value)
-        node.next = head
-        head = node
-        size++
-    }*/
-
     internal fun insertAtFirst(value: T) {
       val node = ListNode(value)
         node.next = head
         head = node
         size++
     }
-
- /*   internal fun getNodeAtIndex(index: Int): ListNode<T>? {
-        if (index < 0) {
-            throw IndexOutOfBoundsException("Index &$index is negative")
-        }
-        if (index > lastIndex) {
-            throw IndexOutOfBoundsException("Index $index passed is more than lastIndex $lastIndex. Hence it is not reachable")
-        }
-        var temp = head
-        var counter = 0
-        while (counter < index) {
-            temp = temp?.next
-            counter++
-        }
-        return temp
-    }*/
 
     internal fun getPreviousNodeOfIndex(index: Int): ListNode<T>? {
         if (index < 0) {
@@ -73,17 +50,6 @@ class LinkedListTest<T> {
         return temp
     }
 
- /*   internal fun insertAtEnd(value: T) {
-        if (size == 0) {
-            insertAtFirst(value)
-            return
-        }
-        val node = ListNode(value)
-        val previous = getNodeAtIndex(lastIndex)
-        previous?.next = node
-        size++
-    }*/
-
     internal fun insertAtEnd(value: T) {
         if (size == 0) { // if the list is empty lastIndex = -1
             insertAtFirst(value)
@@ -94,31 +60,6 @@ class LinkedListTest<T> {
         previous?.next = node
         size++
     }
-
-    /**
-     * A function should always be responsible for validating its own inputs.
-     * That is why validations should be handles inside insertAtIndex() functions rather than relying on
-     * validation from getPreviousNodeOfIndex() function
-     */
-/*    internal fun insertAtIndex(index: Int, value: T) {
-        if (index < 0 || index > size) {
-            throw IndexOutOfBoundsException("Invalid index $index")
-        }
-        if (index == 0) {
-            insertAtFirst(value)
-            return
-        }
-        if (index == size) {
-            insertAtEnd(value)
-            return
-        }
-        val previous = getNodeAtIndex(index - 1)
-        val current = previous?.next
-        val node = ListNode(value)
-        previous?.next = node
-        node.next = current
-        size++
-    }*/
 
     /**
      * A function should always be responsible for validating its own inputs.
